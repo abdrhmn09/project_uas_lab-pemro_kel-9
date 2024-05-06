b9f353cd8aedb2962a5528e41e2d6dd5cf51a8dc
abdrhmn09 <abdurrahmanmarzuki2@gmail.com>
2024-05-06 08:46:22 +0700

    main project

--- project.c
+++ project.c
@@ -1,13 +1,5 @@
 #include <stdio.h>
 #include <string.h>
 int main(){
-    char c[100];
-    // strcpy (c , "c programing");
-    // printf("nilainya %s\n",c);
-    // printf("ukuran c adalah %zu\n",sizeof(c)/sizeof(c[0]));
-    // printf("ukuran c adalah %zu\n",strlen(c));
-    printf("enter name :");
-    // scanf("%s\n",c);
-    fgets(c,sizeof(c),stdin);
-    puts(c);
+    
 }
